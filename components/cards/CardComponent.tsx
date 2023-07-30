a1ae9b298dc2be2f24c62f2a41d0391aa614a05b
Aldi-H <aldihimawan88@gmail.com>
2023-07-30 17:31:00 +0700

    feat: fetch data from store

--- components/cards/CardComponent.tsx
+++ components/cards/CardComponent.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Dimensions, StyleSheet } from 'react-native';
 import { Card, Spacings, Text, Typography, View } from 'react-native-ui-lib';
+import { useChartStore } from '../../store/chartStore';
 
 const CardComponent = () => {
+  const { timeDetailValue, ppmDetailValue } = useChartStore();
+
   Typography.loadTypographies({
     text60BL: { fontSize: 18, fontWeight: '700' },
     text60B: { fontSize: 16, fontWeight: '500' },
@@ -19,12 +22,12 @@ const CardComponent = () => {
       <Card margin-24 paddingV-32 paddingL-24 style={styles.cardContainer}>
         <View row centerV>
           <Text text60BL>Waktu : </Text>
-          <Text text60B>07 Jul 2020</Text>
+          <Text text60B>{timeDetailValue}</Text>
         </View>
 
         <View row centerV>
           <Text text70BL>Kadar Nutrisi : </Text>
-          <Text text70B>10000 ppm</Text>
+          <Text text70B>{ppmDetailValue} ppm</Text>
         </View>
       </Card>
     </View>
