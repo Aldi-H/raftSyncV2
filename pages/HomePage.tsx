a4f58d0e893f677f074f552131c3828c73c239e9
Aldi-H <aldihimawan88@gmail.com>
2023-07-31 14:51:54 +0700

    feat: add refresh on scroll

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import { ScrollView } from 'react-native-gesture-handler';
+import React, { useState, useCallback } from 'react';
+import { RefreshControl, ScrollView } from 'react-native-gesture-handler';
 import ListComponent from '../components/lists/ListComponent';
 
+import { useDeviceStore } from '../store/deviceStore';
+
 const HomePage = () => {
+  const { getAllDevices } = useDeviceStore();
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+    getAllDevices();
+    setRefreshing(false);
+  }, [getAllDevices]);
+
   return (
-    <ScrollView>
+    <ScrollView
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }>
       <ListComponent />
     </ScrollView>
   );
