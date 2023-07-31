90969d26146d69ea7454b820e9e5a37a8d992a6d
Aldi-H <aldihimawan88@gmail.com>
2023-07-31 10:55:08 +0700

    chore: form input

---
+++ components/form/FormComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native-ui-lib';
+
+const FormComponent = () => {
+  return (
+    <View>
+      <Text>Form Component</Text>
+    </View>
+  );
+};
+
+export default FormComponent;
---
+++ pages/AddPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native-ui-lib';
+import FormComponent from '../components/form/FormComponent';
+
+const AddPage = () => {
+  return (
+    <View>
+      <Text>Add page</Text>
+      <FormComponent />
+    </View>
+  );
+};
+
+export default AddPage;
